1935196f2fa8d6847a137170c412c7e7e7fa54cf
sergey sychov <grumx13@gmail.com>
2017-07-30 15:10:24 +0300

    more tests

---
+++ Assets/Test/TestContextResolving.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BehaviourInject.Test
+{
+	public class TestContextResolving : MonoBehaviour
+	{
+		private void Start()
+		{
+			var context = CreateFilledContext();
+
+			Assert.NotNull(context.TestResolve<SimpleDependency>(), "resolve simple");
+			AutocomposedDependency auto = context.TestResolve<AutocomposedDependency>();
+			Assert.NotNull(auto, "resolve auto");
+			Assert.NotNull(auto.ConstructorInjected, "auto constructor injected");
+			Assert.NotNull(auto.PropertyInjected, "auto property injected");
+			Assert.NotNull(auto.FieldInjected, "auto field injected");
+			Assert.NotNull(auto.InitInjected, "auto Init injected");
+
+			Assert.NotNull(context.TestResolve<IDependency>(), "resolve interface");
+			Assert.NotNull(context.TestResolve<IDependencyAuto>(), "resolve auto interface");
+
+			context.Destroy();
+		}
+
+
+		private Context CreateFilledContext()
+		{
+			return Context.Create()
+				.RegisterDependency(new SimpleDependency())
+				.RegisterType<AutocomposedDependency>()
+				.RegisterDependencyAs<IDependencyImpl, IDependency>(new IDependencyImpl())
+				.RegisterTypeAs<IDependencyAutoImpl, IDependencyAuto>();
+		}
+
+		private class SimpleDependency
+		{ }
+
+		private class AutocomposedDependency
+		{
+			public SimpleDependency ConstructorInjected;
+			[Inject]
+			public SimpleDependency PropertyInjected { get; private set; }
+			[Inject]
+			private SimpleDependency _fieldInjected;
+			public SimpleDependency FieldInjected { get { return _fieldInjected; } }
+			public SimpleDependency InitInjected;
+
+			public AutocomposedDependency(SimpleDependency foo)
+			{
+				ConstructorInjected = foo;
+			}
+
+			[Inject]
+			public void Init(SimpleDependency foo)
+			{
+				InitInjected = foo;
+			}
+		}
+
+		private interface IDependency
+		{
+		}
+
+		private class IDependencyImpl : IDependency
+		{
+
+		}
+
+		private interface IDependencyAuto
+		{
+		}
+
+		private class IDependencyAutoImpl : IDependencyAuto
+		{
+
+		}
+	}
+}
--- Assets/Test/TestFactory.cs
+++ Assets/Test/TestFactory.cs
@@ -14,13 +14,13 @@ namespace BehaviourInject.Test
 				.RegisterFactory<Dependency, Factory>();
 
 			Factory factory = (Factory)context.Resolve(typeof(Factory));
-			Assert.NotNull(factory, "factory itself");
+			Assert.NotNull(factory, "factory resolved self");
 
 			Dependency dependency = (Dependency)context.Resolve(typeof(Dependency));
-			Assert.NotNull(dependency, "factored dependency 1");
+			Assert.NotNull(dependency, "factored resolved dependency 1");
 
 			dependency = (Dependency)context.Resolve(typeof(Dependency));
-			Assert.NotNull(dependency, "factored dependency 2 ");
+			Assert.NotNull(dependency, "factored resolved dependency 2 ");
 			Assert.Equals(dependency.Count, 2, "factory creation count");
 
 			context.Destroy();
